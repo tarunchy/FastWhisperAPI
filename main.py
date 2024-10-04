2e2a201bab658700abdb28ca64294095f315bf3a
Tarun Chawdhury <tarunchawdhury@gmail.com>
2024-10-03 19:20:47 -0700

    Added Speaker Diarization

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def initialize_diarization_pipeline():
     
     try:
         diarization_pipeline = Pipeline.from_pretrained(
-            'pyannote/speaker-diarization',
+            'pyannote/speaker-segmentation',
             use_auth_token=hf_auth_token
         )
         logger.info("Diarization pipeline loaded successfully.")
